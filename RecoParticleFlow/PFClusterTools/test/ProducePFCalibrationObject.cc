2672c255be246f07e5a2114f2aa4c9693ab70ef1
bhumika kansal <bhumika.kansal@cern.ch>
2019-01-02 14:42:48 +0100

    No eta dependencies after check

--- RecoParticleFlow/PFClusterTools/test/ProducePFCalibrationObject.cc
+++ RecoParticleFlow/PFClusterTools/test/ProducePFCalibrationObject.cc
@@ -72,6 +72,13 @@ void ProducePFCalibrationObject::beginRun(const edm::Run& run, const edm::EventS
   functType["PFfbEta_BARREL"] = PerformanceResult::PFfbEta_BARREL;
   functType["PFfbEta_ENDCAP"] = PerformanceResult::PFfbEta_ENDCAP;
 
+  functType["PFfcEta_BARRELH"] = PerformanceResult::PFfcEta_BARRELH;
+  functType["PFfcEta_ENDCAPH"] = PerformanceResult::PFfcEta_ENDCAPH;
+  functType["PFfdEta_ENDCAPH"] = PerformanceResult::PFfdEta_ENDCAPH;
+  functType["PFfcEta_BARRELEH"] = PerformanceResult::PFfcEta_BARRELEH;
+  functType["PFfcEta_ENDCAPEH"] = PerformanceResult::PFfcEta_ENDCAPEH;
+  functType["PFfdEta_ENDCAPEH"] = PerformanceResult::PFfdEta_ENDCAPEH;
+
 
   // ---------------------------------------------------------------------------------
   // Write the payload
