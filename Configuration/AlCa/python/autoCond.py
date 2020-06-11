f57ea96104630e8e8e476db1006f6ff065fe998b
tocheng <tocheng.cms@gmail.com>
2020-06-11 23:13:16 +0200

    update to latest offline GT

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -26,9 +26,9 @@ autoCond = {
     # GlobalTag for MC production (p-Pb collisions) with realistic alignment and calibrations for Run2
     'run2_mc_pa'        :   '106X_mcRun2_pA_v5',
     # GlobalTag for Run1 data reprocessing
-    'run1_data'         :   '106X_dataRun2_v27',
+    'run1_data'         :   '106X_dataRun2_v28',
     # GlobalTag for Run2 data reprocessing
-    'run2_data'         :   '106X_dataRun2_v27',
+    'run2_data'         :   '106X_dataRun2_v28',
     # GlobalTag for Run2 data relvals: allows customization to run with fixed L1 menu
     'run2_data_relval'  :   '106X_dataRun2_relval_v25',
     # GlobalTag for Run2 data 2018B relvals only: HEM-15-16 fail
